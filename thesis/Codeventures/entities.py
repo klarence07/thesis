c717223eae2bca90b1d2c29fedc8efca11f01412
klarence07 <idcatindig0586lag@student.fatima.edu.ph>
2025-12-20 01:47:32 +0800

    Delete thesis directory

--- thesis/Codeventures/entities.py
+++
@@ -1,7 +0,0 @@
-class Enemy:
-    def __init__(self, name, health, damage, xp_reward, loot):
-        self.name = name
-        self.health = health
-        self.damage = damage
-        self.xp_reward = xp_reward
-        self.loot = loot
